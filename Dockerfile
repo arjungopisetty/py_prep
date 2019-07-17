28f516ff8d97b562b46208c112a4656e305e27c8
arjungopisetty <arjun.gopisetty@gmail.com>
2019-07-17 14:07:23 -0700

    Update paths

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM python:3
-
-ADD hello_world.py /
-
-CMD [ "python", "./hello_world.py" ]
--- hello_world.py
+++
@@ -1,8 +0,0 @@
-test = 100
-
-def helloworld(test2):
-	print("Hello World!")
-	print(test)
-	print(test2)
-
-helloworld(test2 = 200)
